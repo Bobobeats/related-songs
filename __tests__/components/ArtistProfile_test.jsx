98a6740697adde5ca49cd9a16b420b83152b50af
Andy <xcessivepupil2@gmail.com>
2020-01-27 20:53:28 -0800

    Implement test skeleton for Artist Profile subcomponent

---
+++ __tests__/components/ArtistProfile_test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import RelatedTrack from '../../client/src/components/RelatedTrack';
+import ArtistProfile from '../../client/src/components/ArtistProfile';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Artist Profile Component', () => {
+  describe('should have a following button that can be clicked and will change to reflect following status', () => {
+    test("if not following, upon clicking the state should be changed to 'following'", () => {});
+    test("it should display a message to visit the artist's profile upon hovering on their name", () => {});
+    test('if the artist has a location specified in their profile, the location should be displayed', () => {});
+  });
+});
