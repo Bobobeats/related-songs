238788ac4e844fa4daa0218addf5b8b85445b427
Andy <xcessivepupil2@gmail.com>
2020-01-27 21:34:11 -0800

    Modify the Artist Profile test skelton, implement More test skeleton

--- __tests__/components/ArtistProfile_test.jsx
+++ __tests__/components/ArtistProfile_test.jsx
@@ -9,6 +9,8 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('Artist Profile Component', () => {
   describe('should have a following button that can be clicked and will change to reflect following status', () => {
     test("if not following, upon clicking the state should be changed to 'following'", () => {});
+  });
+  describe('should have other information regarding the artist', () => {
     test("it should display a message to visit the artist's profile upon hovering on their name", () => {});
     test('if the artist has a location specified in their profile, the location should be displayed', () => {});
   });
