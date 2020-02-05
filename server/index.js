5cfd195f327e5838408469e29e083d6721cd4c91
Andy <xcessivepupil2@gmail.com>
2020-02-04 19:04:27 -0800

    add cors middleware

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const db = require('../database/db');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
